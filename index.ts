c4a5844ad2cee25c44caa98c5d98fcb84becefbb
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-02-12 11:02:14 +0530

    add port env

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ app.get("/app", (req: Request, res: Response) => {
   res.send({ data: "this is the res" });
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
   console.log("server listern in port 3000");
 });
